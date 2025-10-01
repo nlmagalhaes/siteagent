46e63d2d55718eba108ff205e0e28916c98ececd
nlmagalhaes <165035204+nlmagalhaes@users.noreply.github.com>
2025-10-01 00:13:58 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,8 @@
-import { Collections } from './sections/Collections';
-import { Events } from './sections/Events';
-import { VisitorArea } from './sections/VisitorArea';
 import { ChatbotButton } from './components/ChatbotButton';
 
 export default function Home() {
   return (
     <div className="space-y-16">
-      <Collections />
-      <Events />
-      <VisitorArea />
       <ChatbotButton />
     </div>
   );
